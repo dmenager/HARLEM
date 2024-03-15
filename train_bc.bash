3fadcf26b26bf3087c2a6eecff6b141ea0cb199b
nhamilton <nathaniel.hamilton.ext@afresearchlab.com>
2024-03-15 18:36:18 -0400

    added taxi to the list

--- train_bc.bash
+++ train_bc.bash
@@ -15,5 +15,12 @@ python behavior_cloning.py --train-expert-hems --env FrozenLake-v1 --eval
 python behavior_cloning.py --train-hems-expert --env FrozenLake-v1 --eval
 python behavior_cloning.py --train-both --env FrozenLake-v1 --eval
 
+# Train Taxi-v3
+python behavior_cloning.py --train-expert --env Taxi-v3 --eval
+python behavior_cloning.py --train-hems --env Taxi-v3 --eval
+python behavior_cloning.py --train-expert-hems --env Taxi-v3 --eval
+python behavior_cloning.py --train-hems-expert --env Taxi-v3 --eval
+python behavior_cloning.py --train-both --env Taxi-v3 --eval
+
 # Evaluate something in particular, swap out the env and load
 python behavior_cloning.py --env CliffWalking-v0 --load expert_trained_CliffWalking-v0.pkl --eval
